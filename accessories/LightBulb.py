9ddc5b238c68d59f3a4eecfb407f99efb6e49b1a
maao <maao@Fermata.local>
2018-06-29 19:04:25 -0700

    Major Update!

--- accessories/LightBulb.py
+++ accessories/LightBulb.py
@@ -5,40 +5,48 @@ import os
 from pyhap.accessory import Accessory
 from pyhap.const import CATEGORY_LIGHTBULB
 
+'''Import Unicorn HAT related libraries'''
+import time
+import unicornhat as unicorn
 
 class LightBulb(Accessory):
 
     category = CATEGORY_LIGHTBULB
 
-    @classmethod
+    '''@classmethod
     def _gpio_setup(_cls, pin):
         print('hi')
         #if GPIO.getmode() is None:
             #GPIO.setmode(GPIO.BOARD)
-        #GPIO.setup(pin, GPIO.OUT)
+        #GPIO.setup(pin, GPIO.OUT)'''
 
-    def __init__(self, *args, pin=11, **kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         serv_light = self.add_preload_service('Lightbulb')
         self.char_on = serv_light.configure_char('On', setter_callback=self.set_bulb)
-
-        #self.pin = pin
-        #self._gpio_setup(pin)
+        '''Initialize Unicorn HAT'''
+        print('Initializing Unicorn HAT')
+        unicorn.set_layout(unicorn.AUTO)
+        unicorn.rotation(0)
+        unicorn.brightness(0.5)
+        width,height=unicorn.get_shape()
 
     def __setstate__(self, state):
         self.__dict__.update(state)
-        #self._gpio_setup(self.pin)
 
     def set_bulb(self, value):
         if value:
-            os.system('chmod +x /home/pi/HAP-python/unicorn.sh')
-            os.system('/home/pi/HAP-python/unicorn.sh &')
-            #GPIO.output(self.pin, GPIO.HIGH)
+            print('INFO: Variable value has been set to', value)
+            for y in range(height):
+                for x in range(width):
+                    unicorn.set_pixel(x,y,255,255,255)
+                    unicorn.show()
+                    time.sleep(0.05)
+
         else:
-            os.system('sudo killall unicorn.sh')
-            #GPIO.output(self.pin, GPIO.LOW)
+            unicorn.clear()
 
     def stop(self):
         super().stop()
-        #GPIO.cleanup()
+        unicorn.clear()
